dfdb5c141e6dfe176006e212494083781150491a
Amaan Baiyat <amaan_baiyat@live.com>
2019-01-25 14:09:28 -0600

    Initial Commit

---
+++ src/main/java/com/abaiyat/triagews/TriageWsApplication.java
@@ -0,0 +1,14 @@
+package com.abaiyat.triagews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriageWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriageWsApplication.class, args);
+	}
+
+}
+
