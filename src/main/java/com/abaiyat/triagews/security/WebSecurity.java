accc38585bcafe53bfd90b0a23819cf9ba4391d9
Amaan Baiyat <amaan_baiyat@live.com>
2019-01-26 22:15:27 -0600

    Added WebSecurity and made Sign-Up url public

---
+++ src/main/java/com/abaiyat/triagews/security/WebSecurity.java
@@ -0,0 +1,15 @@
+package com.abaiyat.triagews.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll();
+    }
+}
