9dd46a35840673acc109bfc058b337617cb6ace4
Amaan Baiyat <amaan_baiyat@live.com>
2019-01-27 14:16:45 -0600

    Implemented Authentication Filter

--- src/main/java/com/abaiyat/triagews/ui/model/request/UserLoginRequestModel.java
+++ src/main/java/com/abaiyat/triagews/ui/model/request/UserLoginRequestModel.java
@@ -1,20 +1,20 @@
 package com.abaiyat.triagews.ui.model.request;
 
 public class UserLoginRequestModel {
-    private String username;
+    private String email;
     private String password;
 
     public UserLoginRequestModel(String username, String password) {
-        this.username = username;
+        this.email = username;
         this.password = password;
     }
 
-    public String getUsername() {
-        return username;
+    public String getEmail() {
+        return email;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {
