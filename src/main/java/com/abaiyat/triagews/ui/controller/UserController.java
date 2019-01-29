7e408ea63c8d711e15e8658f11383a37b388839b
Amaan Baiyat <amaan_baiyat@live.com>
2019-01-29 14:50:42 -0600

    Removed cors config

--- src/main/java/com/abaiyat/triagews/ui/controller/UserController.java
+++ src/main/java/com/abaiyat/triagews/ui/controller/UserController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin(origins = "http:/localhost:8081", maxAge = 3600)
 @RestController
 @RequestMapping("users")
 public class UserController {
